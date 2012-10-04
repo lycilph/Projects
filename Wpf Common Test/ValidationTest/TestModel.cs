1ea19f2229f18faf6c28f6440931b8c9040a713f
Ly Cilph <lycilph@gmail.com>
2012-10-04 13:28:07 +0200

    Added the ValidationTest project

---
+++ Wpf Common Test/ValidationTest/TestModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationTest
+{
+    public class TestModel : ValidationModelBase
+    {
+        public int Prop1
+        {
+            get { return Get(() => Prop1); }
+            set
+            {
+                // Validate property
+                if (value < 10 || value > 100)
+                    AddError("Prop1", "Prop1 must be between 10 and 100");
+                else
+                    RemoveError("Prop1");
+
+                // Update property
+                Set(() => Prop1, value);
+            }
+        }
+
+        public TestModel()
+        {
+            Prop1 = 25;
+        }
+    }
+}
---
+++ Wpf Common Test/ValidationTest/ValidationModelBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using ObservableObjectLibrary;
+using System.Collections;
+
+namespace ValidationTest
+{
+    public class ValidationModelBase : ObservableObject, INotifyDataErrorInfo
+    {
+        private Dictionary<String, String> errors = new Dictionary<String, String>();
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (errors.ContainsKey(propertyName))
+                return errors[propertyName];
+            return null;
+        }
+
+        public bool HasErrors
+        {
+            get { return errors.Count > 0; }
+        }
+
+        public void AddError(string property, string error)
+        {
+            errors[property] = error;
+            NotifyErrorsChanged(property);
+        }
+
+        public void RemoveError(string property)
+        {
+            if (errors.ContainsKey(property))
+                errors.Remove(property);
+            NotifyErrorsChanged(property);
+        }
+
+        public void NotifyErrorsChanged(string property)
+        {
+            var handler = ErrorsChanged;
+            if (handler != null)
+                handler(this, new DataErrorsChangedEventArgs(property));
+        }
+    }
+}
